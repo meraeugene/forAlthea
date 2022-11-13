b5f8ce5b5cfa7c526a5bac3d2188a373a706d90f
arem <andrewvillalon123@gmail.com>
2022-11-13 12:40:27 +0800

    update

--- script.js
+++ script.js
@@ -434,20 +434,20 @@ function togglePlay() {
     player.pause();
   }
 }
-
 // dynamic date
-const timeElement = document.querySelector("#time");
+let timeElement = document.querySelector("#headerTime");
 
-formatTime = (date) => {
+/**
+ * @param {Date} date
+ */
+function formatTime(date) {
   const hours12 = date.getHours() % 12 || 12;
   const minutes = date.getMinutes();
 
   return `${hours12.toString().padStart(2, "0")}:${minutes
     .toString()
     .padStart(2, "0")}`;
-};
+}
 
-setInterval(() => {
-  const now = new Date();
-  timeElement.textContent = formatTime(now);
-}, 200);
+const now = new Date();
+timeElement.textContent = formatTime(now);
